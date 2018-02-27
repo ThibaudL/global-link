6ea02604b75994db290426dc6a5369eb43a6dc22
ThibaudL <lamarchethibaud@hotmail.fr>
2018-02-27 13:01:58 +0100

    log tests

--- src/index.js
+++ src/index.js
@@ -1,55 +1,61 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const pjson = require('./package.json');
-require("create-if-not-exist")('links.json', '{}');
-const links = require('links.json') || {};
 
-const program = require('commander');
+console.log(path.resolve("./links.json"));
+console.log(path.resolve("./package.json"));
+console.log(path.resolve("package.json"));
+console.log(path.resolve("links.json"));
 
-program
-    .version('1.0.0')
-    .usage('<add|remove|use|list> [projectname]')
-    .parse(process.argv);
-
-function writeFile() {
-    fs.writeFile("./links.json", JSON.stringify(links), function (err) {
-        if (err) {
-            return console.log(err);
-        }
-
-        console.log("The file was saved!");
-    });
-}
-
-if (!program.args.length) {
-    program.help();
-} else {
-
-    switch (program.args[0]) {
-        case 'add' :
-            if (links[pjson.name] && links[pjson.name] !== process.argv[1]) {
-                console.warn(`Warning, this project was already registered here : ${links[pjson.name]}`)
-            }
-            links[pjson.name] = process.argv[1];
-            writeFile();
-            break;
-        case 'remove' :
-            delete links[pjson.name];
-            writeFile();
-            break;
-        case 'list' :
-            console.log(JSON.stringify(links));
-            break;
-        case 'use' :
-            if (!program.args[1]) {
-                program.help();
-            } else {
-                process.exit(links[program.args[1]]);
-            }
-            break;
-    }
-
-}
-
-
+// const pjson = require('./package.json');
+// require("create-if-not-exist")('links.json', '{}');
+// const links = require('links.json') || {};
+//
+// const program = require('commander');
+//
+// program
+//     .version('1.0.0')
+//     .usage('<add|remove|use|list> [projectname]')
+//     .parse(process.argv);
+//
+// function writeFile() {
+//     fs.writeFile("./links.json", JSON.stringify(links), function (err) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//
+//         console.log("The file was saved!");
+//     });
+// }
+//
+// if (!program.args.length) {
+//     program.help();
+// } else {
+//
+//     switch (program.args[0]) {
+//         case 'add' :
+//             if (links[pjson.name] && links[pjson.name] !== process.argv[1]) {
+//                 console.warn(`Warning, this project was already registered here : ${links[pjson.name]}`)
+//             }
+//             links[pjson.name] = process.argv[1];
+//             writeFile();
+//             break;
+//         case 'remove' :
+//             delete links[pjson.name];
+//             writeFile();
+//             break;
+//         case 'list' :
+//             console.log(JSON.stringify(links));
+//             break;
+//         case 'use' :
+//             if (!program.args[1]) {
+//                 program.help();
+//             } else {
+//                 process.exit(links[program.args[1]]);
+//             }
+//             break;
+//     }
+//
+// }
+//
+//
