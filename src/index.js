43ca385603733c09a00908c7f78366fb8bcb6b52
ThibaudL <lamarchethibaud@hotmail.fr>
2018-02-27 13:45:11 +0100

    working registering

--- src/index.js
+++ src/index.js
@@ -2,60 +2,55 @@
 const path = require('path');
 const fs = require('fs');
 
-console.log(path.resolve("./links.json"));
-console.log(path.resolve("./package.json"));
-console.log(path.resolve("package.json"));
-console.log(path.resolve("links.json"));
+let globalLinksPath = `${process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']}/AppData/Roaming/npm/globa-links.json`;
+require("create-if-not-exist")(globalLinksPath, '{}');
 
-// const pjson = require('./package.json');
-// require("create-if-not-exist")('links.json', '{}');
-// const links = require('links.json') || {};
-//
-// const program = require('commander');
-//
-// program
-//     .version('1.0.0')
-//     .usage('<add|remove|use|list> [projectname]')
-//     .parse(process.argv);
-//
-// function writeFile() {
-//     fs.writeFile("./links.json", JSON.stringify(links), function (err) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//
-//         console.log("The file was saved!");
-//     });
-// }
-//
-// if (!program.args.length) {
-//     program.help();
-// } else {
-//
-//     switch (program.args[0]) {
-//         case 'add' :
-//             if (links[pjson.name] && links[pjson.name] !== process.argv[1]) {
-//                 console.warn(`Warning, this project was already registered here : ${links[pjson.name]}`)
-//             }
-//             links[pjson.name] = process.argv[1];
-//             writeFile();
-//             break;
-//         case 'remove' :
-//             delete links[pjson.name];
-//             writeFile();
-//             break;
-//         case 'list' :
-//             console.log(JSON.stringify(links));
-//             break;
-//         case 'use' :
-//             if (!program.args[1]) {
-//                 program.help();
-//             } else {
-//                 process.exit(links[program.args[1]]);
-//             }
-//             break;
-//     }
-//
-// }
-//
-//
+const pjson = require(path.resolve('./package.json'));
+const links = require(globalLinksPath) || {};
+
+const program = require('commander');
+
+program
+    .version('1.0.0')
+    .usage('<add|remove|use|list> [projectname]')
+    .parse(process.argv);
+
+function writeFile() {
+    console.log(links);
+    fs.writeFile(globalLinksPath, JSON.stringify(links), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Config file updated !");
+    });
+}
+
+if (!program.args.length) {
+    program.help();
+} else {
+
+    switch (program.args[0]) {
+        case 'add' :
+            if (links[pjson.name] && links[pjson.name] !== process.argv[1]) {
+                console.warn(`Warning, this project was already registered here : ${links[pjson.name]}`)
+            }
+            links[pjson.name] = process.argv[1];
+            writeFile();
+            break;
+        case 'remove' :
+            delete links[pjson.name];
+            writeFile();
+            break;
+        case 'list' :
+            console.log(JSON.stringify(links));
+            break;
+        case 'use' :
+            if (!program.args[1]) {
+                program.help();
+            } else {
+                console.log(links[program.args[1]]);
+            }
+            break;
+    }
+
+}
